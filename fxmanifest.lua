4b071b438831a0c433dd8815462d60a6f3eea998
DxrkGhostIx <n.n.rasewerg@outlook.de>
2026-04-28 00:15:43 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DxrkGhost'
+description 'Motor vehicle HUD for cars/trucks/planes/helis and separate bike computer HUD'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/assets/bike_computer.png'
+}
+
+client_script 'client.lua'
